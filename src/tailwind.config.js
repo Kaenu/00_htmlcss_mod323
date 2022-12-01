3abee7e16e141a4c004b2fda3e9a89860bedc618
DESKTOP-5CIGU2G\kaenu <kanusan1@hotmail.ch>
2022-12-01 21:01:03 +0100

    Worksheet: Introduction HTML & CSS done

---
+++ src/tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    theme: {
+      colors: {
+        'gray-700': '#c2b7b778',
+        'gray-200': '#c8c8c8'
+      },
+      fontFamily: {
+        sans: 'sans-serif'
+      },
+      fontSize: {
+        '2xl': '36px',
+        'base': '18px'
+      },
+      lineHeight: {
+        'normal': '1.5'
+      },
+      spacing: {
+        '4': '20px',
+        '10': '10px'
+      }
+    },
+    variants: {},
+    plugins: []
+  }
